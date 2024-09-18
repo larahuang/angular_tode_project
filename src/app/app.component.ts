db7721999c795cda9813f33bb66a4524182a8e14
lara huang <lara1105huang@gmail.com>
2024-09-18 13:48:48 +0800

    表單與按鈕綁定函式的執行

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import { ButtonModule } from 'primeng/button';
 import { ToDoComponent } from './components/to-do/to-do.component';
 
 @Component({
@@ -7,6 +9,8 @@ import { ToDoComponent } from './components/to-do/to-do.component';
   standalone: true,
   imports: [
     RouterOutlet,
+    MatButtonModule,
+    ButtonModule,
     ToDoComponent,
   ],
   templateUrl: './app.component.html',
--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.scss'
 })
 export class ToDoComponent {
   Title: string = 'To Do Lists';
-  imageLink:string= "https://cdn-icons-png.flaticon.com/512/4697/4697260.png";
+  imageLink: string = "https://cdn-icons-png.flaticon.com/512/4697/4697260.png";
+  tasks : string[] =[];
+  newTask: string = "";//表單綁定
+  isAvalible : boolean = false;
+  addTasks() {
+    const vm = this;
+    if (vm.tasks != null) {
+      vm.tasks.push(vm.newTask)
+      vm.newTask="";
+      vm.isAvalible = true;
+    }
+    console.log(vm.tasks)
+  }
 }
