c7553de0802edca4959415629302a4def04d720b
lara huang <lara1105huang@gmail.com>
2024-09-24 16:54:34 +0800

    nav li active

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterLink} from '@angular/router';
+import { CommonModule,Location } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+interface navListType{
+  title: string,
+  path: string,
+}
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar', //這是元件的名稱
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,CommonModule,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  navLists: navListType[] = [
+    { title: 'Home' ,path:'index'},
+    {title:'FetchData',path:'fetch-data'}
+  ]
+  //頁面名稱
+  pageTitle: string = 'Home';
+
+  //
+  isActive: boolean = false;
+
+  //點擊頁面按鈕函式
+  actionList(path: string) {
+    this.pageTitle = path;
+  }
 }
